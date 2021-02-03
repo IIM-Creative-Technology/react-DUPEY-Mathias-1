d37bde5a8a3e45190ae2814593769dc5e5855cec
Toshiro MPIKA <49293384+ToshhM@users.noreply.github.com>
2021-02-03 15:03:37 +0100

    Navbar
    Création de la navbar du projet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Home from './components/Landing/Home'
+import Navbar from './components/Header/Navbar'
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch,} from "react-router-dom"
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      </Router>
     </div>
   );
 }
