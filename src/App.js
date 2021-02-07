d1ab20cbc42c923793a864027c01b7e7cb174725
DUPEY Mathias <71030042+DUPEY-Mathias@users.noreply.github.com>
2021-02-07 21:07:47 +0100

    Qui Sommes Nous ?

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react'
 import Home from './components/Landing/Home'
 import Navbar from './components/Header/Navbar'
 import PresentationQuizz from './components/Quizz/PresentationQuizz'
+import Nous from './components/Nous/Nous'
 import NoMatch from './components/404/NoMatch'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, } from "react-router-dom"
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/PresentationQuizz" exact component={PresentationQuizz} />
           <Route path="/Quizz" exact component={Quizz} />
+          <Route path="/Nous" exact component={Nous} />
           <Route path="*" exact component={NoMatch} />
         </Switch>
       </Router>
---
+++ src/components/Nous/Nous.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Nous.css";
+
+function Nous() {
+    return (
+        <div class="Nous">
+            <h1>Qui somme nous ?</h1>
+            <p>Matoshiro, Toshias, Mathias, Thosiro qu'est ce que tu attendais de mieux ?</p>
+            <p>Étudiants, nous parcourons le vaste internet à la recherche de pépites diverses et variées.</p>
+        </div>
+    );
+}
+
+export default Nous;
